1d52def54f757e8637369289c07b0465b133135e
Tushjenny <oguguajenny25@.com>
2023-02-17 08:00:14 -0800

    4-puts.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
-#Include < stdio.h >
+#include <stdio.h>
 /**
  * main . A program that prints a line using the printf function
  * Return: 0 (success)
  */
-int main(void)
+int main(void) 
 {
 	printf("with proper grammar, but the outcome is a piece of art, \n")
 	return (0);
