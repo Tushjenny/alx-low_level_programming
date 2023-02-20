48379cf1cb61782a9235545909e5147007d42a87
Tushjenny <oguguajenny25@.com>
2023-02-20 13:03:00 -0800

    task 6

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 1 (success)
+ */
+
+int main(void)
+{
+fwrite("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19"\n", 61);
+return (1);
+}
