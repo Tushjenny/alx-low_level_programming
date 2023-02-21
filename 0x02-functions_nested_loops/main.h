27afaf47d00bd9c4aa1cb3c0be52d151c16abc0f
Tushjenny <oguguajenny25@.com>
2023-02-21 10:07:11 -0800

    0-putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-void print_alphabet(void);
-void print_alphabet_x10(void);
+int _putchar(char c);
+void print_alphabeth(void);
+void print_alphabeth_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -12,5 +10,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-
-#endif
+void print_times(int n);
