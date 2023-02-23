e4bf33806c84225e51a28b6a4eceb30e2ab52479
Tushjenny <oguguajenny25@.com>
2023-02-23 13:43:04 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,14 @@
 /**
   * print_numbers - Print the numbers since 0 up to 9
   *
-  * Return: The numbers since 0 up to 9
+  * Return: no return
   */
 void print_numbers(void)
+
 {
 	int ch;
 
-	for (ch = 0; ch <= 9; ch++)
+	for (ch = 48; ch <= 58; ch++)
 	{
 		_putchar(ch);
 	}
